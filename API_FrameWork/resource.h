c10c1b87ed7e7e8b17a79552024790df581762c4
DESKTOP-PI4VQ9N\Hyungil Kim <samljkh@naver.com>
2021-09-11 12:19:56 +0900

    no message

--- API_FrameWork/resource.h
+++ API_FrameWork/resource.h
@@ -12,8 +12,8 @@
 #define IDI_SMALL                       108
 #define IDC_APIFRAMEWORK                109
 #define IDR_MAINFRAME                   128
-#define IDI_ICON1                       131
-#define IDC_STATIC                      -1
+#define IDI_ICON1                       1
+#define IDC_STATIC                      101
 
 // Next default values for new objects
 // 
